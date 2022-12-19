72626d9700a623573be154e1f892d3592dae4f9c
Martin Andrej Johan Grönlund <3044707+majg0@users.noreply.github.com>
2022-12-19 20:51:14 +0100

    feat(nvim): completion

--- nvim/config/init.lua
+++ nvim/config/init.lua
@@ -14,7 +14,14 @@ require('lualine').setup {
 
 require("nvim-tree").setup()
 
-local cmp = require("cmp")
+-- Setup completion
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local luasnip = require('luasnip')
+local cmp = require('cmp')
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -26,24 +33,34 @@ cmp.setup({
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback()
       end
     end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function()
+
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
       end
     end, { 'i', 's' }),
   },
   sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "nvim_lsp_signature_help" },
-    { name = "path" },
+    { name = 'nvim_lsp' },
+    { name = 'nvim_lsp_signature_help' },
+    { name = 'path' },
   }, {
-    { name = "buffer", keyword_length = 3 },
+    { name = 'buffer', keyword_length = 3 },
   }),
 })
+
 
 -- Setup buffer-local keymaps / options for LSP buffers
 local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
