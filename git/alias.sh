57e054857848d577b01c07b7fc86c97ce340a39c
Martin Andrej Johan Grönlund <martingronlund@live.se>
2023-01-05 15:21:59 +0100

    feat(git): use dev/prod instead of develop

--- git/alias.sh
+++ git/alias.sh
@@ -1,19 +1,24 @@
-## add
+# add
 alias ga='git add'
-alias gaa='git add -A'
-alias gap='git add -p'
+alias gaa='git add --all'
+alias gap='git add --patch'
 
-## checkout
+# branch
+alias gb='git branch'
+alias gbd='git branch --delete'
+
+# checkout
 alias gco='git checkout'
 alias gcob='git checkout -b'
-alias gcod='git checkout develop'
+alias gcod='git checkout dev'
 alias gcom='git checkout main'
 alias gcomr='git checkout master'
+alias gcop='git checkout prod'
 
-## clean
+# clean
 alias gnfd='git clean -fd'
 
-## commit
+# commit
 alias gc='git commit'
 alias gca='git commit --amend'
 alias gcf='git commit --fixup'
@@ -23,59 +28,58 @@ alias gcna='git commit --no-verify --amend'
 alias gcnf='git commit --no-verify --fixup'
 alias gcnm='git commit --no-verify --message'
 
-## cherry-pick
+# cherry-pick
 alias gcp='git cherry-pick'
 alias gcpc='git cherry-pick --continue'
 alias gcps='git cherry-pick --skip'
 alias gcpa='git cherry-pick --abort'
 
-## diff
+# diff
 alias gd='git diff'
 alias gdc='git diff --cached'
 
-## log
+# log
 alias gl='git log'
 
-## merge
-alias gmod='git merge origin/develop'
+# merge
+alias gmod='git merge origin/dev'
 
-## pull
+# pull
 alias gp='git pull'
 alias gpo='git pull origin'
-alias gpod='git pull origin develop'
+alias gpod='git pull origin dev'
 alias gpa='git pull --autostash'
 alias gpao='git pull --autostash origin'
-alias gpaod='git pull --autostash origin develop'
+alias gpaod='git pull --autostash origin dev'
 alias gpr='git pull --rebase'
 alias gpro='git pull --rebase origin'
-alias gprod='git pull --rebase origin develop'
+alias gprod='git pull --rebase origin dev'
 alias gpra='git pull --rebase --autostash'
 alias gprao='git pull --rebase --autostash origin'
-alias gpraod='git pull --rebase --autostash origin develop'
+alias gpraod='git pull --rebase --autostash origin dev'
 
-## push
+# push
 alias gu='git push'
 alias guf='git push --force-with-lease'
 
-## rebase
+# rebase
 alias griaa='git rebase -i --autosquash --autostash'
 alias grc='git rebase --continue'
 
-## reset
+# reset
 alias gt='git reset'
 alias gth='git reset --hard'
-alias gthod='git reset --hard origin/develop'
+alias gthod='git reset --hard origin/dev'
 
-## show
+# show
 alias gw='git show'
 
-## stash
+# stash
 alias gx='git stash'
 alias gxd='git stash drop'
 alias gxl='git stash list'
 alias gxp='git stash pop'
 alias gxs='git stash show'
 
-## status
+# status
 alias gs='git status'
-
