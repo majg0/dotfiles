cdd17d228f66a01e6abe8264941c9f96f3fd0c4b
Martin Andrej Johan Grönlund <martingronlund@live.se>
2023-05-05 08:48:51 +0200

    feat(nvim)!: disable chatgpt key by default

--- nvim/config/lua/plugins.lua
+++ nvim/config/lua/plugins.lua
@@ -131,34 +131,6 @@ require("packer").startup({
 		use("MunifTanjim/nui.nvim")
 
 		use({
-			"jackMort/ChatGPT.nvim",
-			config = function()
-				require("chatgpt").setup({
-					openai_params = {
-						model = "gpt-3.5-turbo",
-						frequency_penalty = 0,
-						presence_penalty = 0,
-						max_tokens = 500,
-						temperature = 0,
-						top_p = 1,
-						n = 1,
-					},
-					openai_edit_params = {
-						model = "gpt-3.5-turbo",
-						temperature = 0,
-						top_p = 1,
-						n = 1,
-					},
-				})
-			end,
-			requires = {
-				"MunifTanjim/nui.nvim",
-				"nvim-lua/plenary.nvim",
-				"nvim-telescope/telescope.nvim",
-			},
-		})
-
-		use({
 			"mxsdev/nvim-dap-vscode-js",
 			requires = { "mfussenegger/nvim-dap" },
 		})
--- nvim/config/plugin/packer_compiled.lua
+++ nvim/config/plugin/packer_compiled.lua
@@ -74,12 +74,6 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
-  ["ChatGPT.nvim"] = {
-    config = { "\27LJ\2\n„\2\0\0\4\0\b\0\v6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\0025\3\6\0=\3\a\2B\0\2\1K\0\1\0\23openai_edit_params\1\0\4\6n\3\1\ntop_p\3\1\16temperature\3\0\nmodel\18gpt-3.5-turbo\18openai_params\1\0\0\1\0\a\21presence_penalty\3\0\22frequency_penalty\3\0\6n\3\1\nmodel\18gpt-3.5-turbo\ntop_p\3\1\16temperature\3\0\15max_tokens\3ô\3\nsetup\fchatgpt\frequire\0" },
-    loaded = true,
-    path = "/Users/martin.gronlund/.config/local/share/nvim/site/pack/packer/start/ChatGPT.nvim",
-    url = "https://github.com/jackMort/ChatGPT.nvim"
-  },
   LuaSnip = {
     loaded = true,
     path = "/Users/martin.gronlund/.config/local/share/nvim/site/pack/packer/start/LuaSnip",
@@ -264,22 +258,6 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: ChatGPT.nvim
-time([[Config for ChatGPT.nvim]], true)
-try_loadstring("\27LJ\2\n„\2\0\0\4\0\b\0\v6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\0025\3\6\0=\3\a\2B\0\2\1K\0\1\0\23openai_edit_params\1\0\4\6n\3\1\ntop_p\3\1\16temperature\3\0\nmodel\18gpt-3.5-turbo\18openai_params\1\0\0\1\0\a\21presence_penalty\3\0\22frequency_penalty\3\0\6n\3\1\nmodel\18gpt-3.5-turbo\ntop_p\3\1\16temperature\3\0\15max_tokens\3ô\3\nsetup\fchatgpt\frequire\0", "config", "ChatGPT.nvim")
-time([[Config for ChatGPT.nvim]], false)
--- Config for: telescope-dap.nvim
-time([[Config for telescope-dap.nvim]], true)
-try_loadstring("\27LJ\2\nH\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0'\2\3\0B\0\2\1K\0\1\0\bdap\19load_extension\14telescope\frequire\0", "config", "telescope-dap.nvim")
-time([[Config for telescope-dap.nvim]], false)
--- Config for: playground
-time([[Config for playground]], true)
-try_loadstring("\27LJ\2\n/\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\20nvim-treesitter\frequire\0", "config", "playground")
-time([[Config for playground]], false)
--- Config for: crates.nvim
-time([[Config for crates.nvim]], true)
-try_loadstring("\27LJ\2\n4\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\vcrates\frequire\0", "config", "crates.nvim")
-time([[Config for crates.nvim]], false)
 -- Config for: gitsigns.nvim
 time([[Config for gitsigns.nvim]], true)
 try_loadstring("\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rgitsigns\frequire\0", "config", "gitsigns.nvim")
@@ -292,6 +270,18 @@ time([[Config for nvim-surround]], false)
 time([[Config for trouble.nvim]], true)
 try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\ftrouble\frequire\0", "config", "trouble.nvim")
 time([[Config for trouble.nvim]], false)
+-- Config for: crates.nvim
+time([[Config for crates.nvim]], true)
+try_loadstring("\27LJ\2\n4\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\vcrates\frequire\0", "config", "crates.nvim")
+time([[Config for crates.nvim]], false)
+-- Config for: playground
+time([[Config for playground]], true)
+try_loadstring("\27LJ\2\n/\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\20nvim-treesitter\frequire\0", "config", "playground")
+time([[Config for playground]], false)
+-- Config for: telescope-dap.nvim
+time([[Config for telescope-dap.nvim]], true)
+try_loadstring("\27LJ\2\nH\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0'\2\3\0B\0\2\1K\0\1\0\bdap\19load_extension\14telescope\frequire\0", "config", "telescope-dap.nvim")
+time([[Config for telescope-dap.nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
