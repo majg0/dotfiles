5be818e266477c926fa04e0d6b5ec68a5751d119
Martin Andrej Johan Grönlund <martingronlund@live.se>
2023-05-16 13:57:26 +0200

    feat(op): source plugins
    this enables e.g. gh cli to auth without storing files locally

---
+++ op/rc.sh
@@ -0,0 +1,3 @@
+# NOTE: not sure what happens without any enabled plugins;
+# may require following e.g. https://developer.1password.com/docs/cli/shell-plugins/github/
+source "$XDG_CONFIG_HOME/op/plugins.sh"
