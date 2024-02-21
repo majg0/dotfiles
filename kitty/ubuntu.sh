cfaba6a01fd69c08c699594e8a8eeced30a11ece
Martin Grönlund <martin@intuicell.com>
2024-02-21 08:21:12 +0100

    fix(kitty): no launch post-install

--- kitty/ubuntu.sh
+++ kitty/ubuntu.sh
@@ -1,1 +1,1 @@
-curl --proto '=https' --tlsv1.2 -sSf https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+curl --proto '=https' --tlsv1.2 -sSf https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n
