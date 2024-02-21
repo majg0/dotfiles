c9fac55380944e8e4f51e27f12782b31fb80a3b0
Martin Grönlund <martin@intuicell.com>
2024-02-21 08:23:31 +0100

    feat(nnn): cd, show hidden & details

--- nnn/alias.sh
+++ nnn/alias.sh
@@ -1,1 +1,15 @@
-alias l='nnn -e'
+alias l='nnn'
+
+n () # to cd on quit
+{
+    if [ -n $NNNLVL ] && [ "${NNNLVL:-0}" -ge 1 ]; then
+        echo "nnn is already running"
+        return
+    fi
+    export NNN_TMPFILE="$XDG_CONFIG_HOME/nnn/.lastd"
+    nnn "$@"
+    if [ -f "$NNN_TMPFILE" ]; then
+            . "$NNN_TMPFILE"
+            rm -f "$NNN_TMPFILE" > /dev/null
+    fi
+}
--- nnn/rc.sh
+++ nnn/rc.sh
@@ -1,2 +1,3 @@
 export NNN_PLUG='u:davecloud;o:fzopen;c:fcd;j:jump;p:preview-tui;i:imgview;v:vidthumb'
 export NNN_FIFO='/tmp/nnn.fifo'
+export NNN_OPTS='deH' # d=show details, e=open with $VISUAL, H=show hidden files
