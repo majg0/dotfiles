a402804a07c5ca1c9cb7a7f99cf73c8d10869624
Martin <martingronlund@live.se>
2024-02-23 13:52:55 +0100

    feat(fonts): install Hasklig nerd font

---
+++ fonts/ubuntu.sh
@@ -0,0 +1,8 @@
+font=Hasklig
+target="/usr/share/fonts/opentype/${font}"
+if [ ! -d $target ]; then
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/${font}.zip
+  unzip ${font}.zip -d ${font}
+  rm ${font}.zip
+  sudo mv ${font} ${target}
+fi
