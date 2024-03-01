237f6a90d496faa864c94870f17091af126c6378
Martin Andrej Johan Grönlund <martingronlund@live.se>
2024-03-01 08:17:06 +0100

    feat(rustup): add rust-analyzer on install

--- rustup/ubuntu.sh
+++ rustup/ubuntu.sh
@@ -1,3 +1,5 @@
 source "$modpath/env.sh"
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | RUSTUP_INIT_SKIP_PATH_CHECK=yes sh -s -- --no-modify-path -y
+
+rustup component add rust-analyzer
