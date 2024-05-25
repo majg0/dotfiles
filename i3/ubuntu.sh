eb3025922e791ac39aa2bb3ed0e5a9f756d24fa4
Martin Andrej Johan Grönlund <martingronlund@live.se>
2024-05-25 09:22:59 +0200

    fix(i3): allow auth requests via polkit so we can run e.g. snap store

--- i3/ubuntu.sh
+++ i3/ubuntu.sh
@@ -1,4 +1,4 @@
-sudo apt --yes install i3 pulseaudio-utils
+sudo apt --yes install i3 pulseaudio-utils lxpolkit
 sudo update-alternatives --install /usr/bin/x-session-manager x-session-manager $(which i3) 60
 if [ $XDG_CURRENT_DESKTOP != i3 ]; then
   notify "${light_cyan}Log back in to use i3!${default}"
