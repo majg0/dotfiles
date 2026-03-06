32d58789dbd75e008efa3c210177a6c2df29e19b
Martin Andrej Johan Grönlund <martingronlund@live.se>
2026-03-06 12:00:27 +0100

    chore: update SA token key

--- claude/rc.sh
+++ claude/rc.sh
@@ -2,6 +2,6 @@ claude() {
   CLAUDE_TTY=$(tty 2>/dev/null) \
   GIT_CONFIG_GLOBAL=~/.config/git/claude \
   GIT_SSH_COMMAND="$HOME/.claude/git_ssh.sh" \
-  OP_SERVICE_ACCOUNT_TOKEN="${OP_SERVICE_ACCOUNT_TOKEN:-$(op item get "Service Account Auth Token: Claude" --vault Private --field credential --reveal)}" \
+  OP_SERVICE_ACCOUNT_TOKEN="${OP_SERVICE_ACCOUNT_TOKEN:-$(op item get "Service Account Auth Token: Agents" --vault Private --field credential --reveal)}" \
   command claude "$@"
 }
